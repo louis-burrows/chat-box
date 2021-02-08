b02105372b4b8f8333665a9b0603b8ad603f5dc5
Louis Burrows <you@example.com>
2021-02-08 10:51:21 +0000

    added checkbox

---
+++ containers/HowManyParticipants.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+const Input: React.FC = ({ }): JSX.Element => {
+
+
+
+  return (
+    <>
+      <div>
+      
+      </div>
+    </>
+  )
+}
+
+export default Input;
